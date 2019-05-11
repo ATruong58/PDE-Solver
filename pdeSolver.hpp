c88fa6beb1f9cac2a8b6e1efa473801ef7f71d40
Alan Truong <alan@Alans-MacBook-Pro.local>
2019-05-11 01:00:10 -0500

    Finish B vector

--- pdeSolver.hpp
+++ pdeSolver.hpp
@@ -4,23 +4,45 @@ myvector<T> pdeSolver<T,U>::operator()(pdeProblem<T,U> &P, int n)
 
     double stepSize = (1.0 / n) * -1.0;
     int size = pow(n-1,2);
-    symmetricMatrix<T> a(size);
-    std::cout<< 6 % 3 << std::endl;
+    symmetricMatrix<T> a_matrix(size);
+    myvector<T> b_vector(size);
+    int count = 2;
+
     for(int i = 0; i < size; i++)
     {
-        a(i,i) = 1;
+        a_matrix(i,i) = 1;
         
         if(n+i <= size)
         {
-            a(i,n+i-1) = stepSize;
+            a_matrix(i,n+i-1) = stepSize;
         }
         if((i + 1) % (n - 1) != 0 && i+1 < size)
         {
-            a(i,i+1) = stepSize;
+            a_matrix(i,i+1) = stepSize;
         }
     }
 
-    std::cout << a << std::endl; 
+    for(int i = 0; i < size; i++)
+    {
+        if(i == 0)
+        {
+            b_vector[i] = 2 * P(M_PI/n);
+        }
+        else if(i > 0 && i < n - 1)
+        {
+            b_vector[i] = P(((i+1) * M_PI) / n);
+        }
+        else if( i >= (n - 1) && i % (n - 1) == 0)
+        {
+            b_vector[i] = P((count * M_PI) / n );
+            count++;
+        }
+        else
+        {
+            b_vector[i] = 0;
+        }
+    }
+    std::cout << b_vector << std::endl; 
 
 
 
